b813ff3b31f4323ffdf4a30848063a727a930244
chris nyola <chrisskye12@yahoo.com>
2024-02-26 16:50:58 -0600

    homework 2.7

--- 2.7/2.7Test.test.ts
+++ 2.7/2.7Test.test.ts
@@ -3,14 +3,37 @@ const page = new pageObject();
 
 class Intern {
    //create a class for interns that includes the name phone and title for the intern.
+   name: string;
+   phone: number;
+   title: string;
+
+   constructor(name: string, phone: number, title: string) {
+      this.name = name,
+      this.phone = phone,
+      this.title = title
+   }
 }; 
 
 let newInterns: Array<Intern> = [
    //create an array of Interns using your class 
+   new Intern("Thierry", 1234998761, "CEO"),
+   new Intern("Erin", 2345998762, "VP CEO"),
+   new Intern("Elvis", 3234498763, "Engineer"),
+   new Intern("tina", 1234998765, "Secretary"),
+   new Intern("Gerry", 4321998766, "Acountant"),
+
 ];
 
 let addInterns = async (newInterns) => {
    //create a function to add the interns from your array
+   await pages.click(pages.addEmployee);
+   await pages.click(pages.newEmployee);
+   await pages.setInput(pages.namInp, newInterns.name); 
+   await pages.setInput(pages.phoInp, newInterns.phone); 
+   await pages.setInput(pages.titleInp, newInterns.title); 
+   await pages.click(pages.saveBtn); 
+   await pages.driver.sleep(3000); 
+
 };
 
 test('can add the bake off crew',async () => {
